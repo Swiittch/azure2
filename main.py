cdfb567384425ba8af4a7b2fe37e87a92ec27efd
Swiittch <33943840+Swiittch@users.noreply.github.com>
2021-11-07 14:26:30 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, url_for
  
 app = Flask(__name__)
- 
+
+app.add_url_rule('/photos/<path:filename>', endpoint='photos', view_func=app.send_static_file) 
 @app.route('/')
 def index():
    return render_template("index.html")
  
-
 if __name__ == '__main__':
     app.run(debug=True)
